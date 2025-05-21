fa9a3a7cbcfc2e3dd6c32d6e7403e486ad562f66
Gayatri <gaikwadgayatri308@gmail.com>
2025-05-21 17:41:27 +0530

    updated

--- src/main/java/com/inn/cafe/com/inn/cafe/POJO/Category.java
+++ src/main/java/com/inn/cafe/com/inn/cafe/POJO/Category.java
@@ -7,16 +7,20 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import java.io.Serializable;
 
-@NamedQuery (name = "Category.getAllCategory" ,query ="select category c from c")
+//@NamedQuery (name = "Category.getAllCategory" ,query ="select Category c from c")
+@NamedQuery(name = "Category.getAllCategory", query = "SELECT c FROM Category c")
+
 
 @Data
 @Entity
 @DynamicUpdate
 @DynamicInsert
-
+@Table(name = "category")
 
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     @Column(name ="id")
     private Integer id;
@@ -24,7 +28,7 @@ public class Category implements Serializable {
     @Column(name = "name")
     private String name;
 
-
-
-
 }
+
+
+
